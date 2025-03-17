705089c48d056a5f7d21b8393867ea6c82456d32
YANG Jackie <asphyxytb@gmail.com>
2025-03-17 09:55:19 +0100

    aaa

--- ProgC/TP7/ptr.c
+++ ProgC/TP7/ptr.c
@@ -1,35 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-int* f(int n) {
-    int j;
-    int* q;
-    printf("  0 - In f: %p, %d\n", &n, n);
-    q = &j;
-    printf("  1 - In f: %p, %d\n", q, *q);
-    j = n;
-    printf("  2 - In f: %p, %d\n", q, *q);
-    return q;
+void f(){
+  int* p;
+  p = malloc(100 * sizeof(int));
+
+  if (p != NULL){
+    printf("true \n");
+  } else {
+    printf("false \n");
   }
+}
    
-  void* g(int n) {
-    int k;
-    int* r;
-    printf("  0 - In g: %p, %d\n", &n, n);
-    r = &k;
-    printf("  1 - In g: %p, %d\n", r, *r);
-    k = n;
-    printf("  2 - In g: %p, %d\n", r, *r);
-    return NULL;
-  }
-   
-  int main() {
-    int i = 0;
-    int* p = &i;
-    printf("Before f: %p, %d\n", p, *p);
-    p = f(1234);
-    printf("After f: %p, %d\n", p, *p);
-    g(4321);
-    printf("After g: %p, %d\n", p, *p);
+int main() {
+    int *p = NULL;
+    f();
+    printf("Contenu de *p : %d\n", *p);
     return EXIT_SUCCESS;
-  } 
+} 
